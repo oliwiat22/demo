01c7b822d9676fcd7d08f4f896edf089e8a8099d
wika <szendzielorz.wik@protonmail.com>
2023-05-26 19:22:59 +0200

    kod

---
+++ src/main/java/com/example/demo/user/UpdateUserRequest.java
@@ -0,0 +1,27 @@
+package com.example.demo.user;
+
+public class UpdateUserRequest {
+    private String password;
+    private String username;
+
+    public UpdateUserRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
